e9f2cbd7d79766142eef3b512502dd065812a9db
Rui Mao <ruimao18@gmail.com>
2017-04-07 22:34:49 -0700

    fixed loading data before have to tap the search bar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -15,13 +15,13 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     var searchController: UISearchController!
     
     @IBOutlet weak var tableView: UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.tableView.dataSource = self
         self.tableView.delegate = self
-
+        
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 120
         
@@ -33,58 +33,57 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         definesPresentationContext = true
         
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
-            
             self.businesses = businesses
             if let businesses = businesses {
                 for business in businesses {
-                    self.tableView.reloadData()
                     print(business.name!)
                     print(business.address!)
                 }
+                self.filteredData = self.businesses
+                self.tableView.reloadData()
             }
-            
-            }
-        )
-        //filteredData = (businesses?)!
-        }
-
+        })
+    }
+    
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection: Int) -> Int {
-            /*if businesses != nil {
-            return businesses!.count
-            }
-            else {
-                return 0
-            }*/
+        /*if businesses != nil {
+         return businesses!.count
+         }
+         else {
+         return 0
+         }*/
         return filteredData.count
-        }
-
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath as IndexPath) as! BusinessCell
-            
-            cell.business = filteredData[indexPath.row]
-            return cell
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath as IndexPath) as! BusinessCell
+        
+        cell.business = filteredData[indexPath.row]
+        return cell
+    }
     
     func updateSearchResults(for searchController: UISearchController) {
         if let searchText = searchController.searchBar.text {
             self.filteredData = searchText.isEmpty ? self.businesses! : self.businesses.filter({(business: Business) -> Bool in
                 return (business.name)?.range(of: searchText, options: .caseInsensitive) != nil
-                })
+            })
             tableView.reloadData()
         }
     }
     
-        /* Example of Yelp search with more search options specified
-         Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
-         self.businesses = businesses
-         
-         for business in businesses {
-         print(business.name!)
-         print(business.address!)
-         }
-         }
-         */
-        
+    /* Example of Yelp search with more search options specified
+     Business.searchWithTerm("Restaurants", sort: .Distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: NSError!) -> Void in
+     self.businesses = businesses
+     
+     for business in businesses {
+     print(business.name!)
+     print(business.address!)
+     }
+     }
+     */
+    
     
     
     override func didReceiveMemoryWarning() {
@@ -94,13 +93,13 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
     
     
     
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let navigationController = segue.destination as! UINavigationController
         let filterViewController =
-        navigationController.topViewController as! filterViewController
+            navigationController.topViewController as! filterViewController
         filterViewController.delegate = self
     }
     
@@ -109,6 +108,7 @@ class BusinessesViewController: UIViewController, UITableViewDataSource, UITable
         Business.searchWithTerm(term: "Restaurants", sort: nil, categories: categories, deals: nil) {
             (businesses: [Business]?, error: Error?) -> Void in
             self.businesses = businesses
+            self.filteredData = self.businesses
             self.tableView.reloadData()
         }
     }
